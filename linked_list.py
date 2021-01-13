798f8b1732dd3b0715ee44222f4c99b3cb09d045
jtreeves <jackson.t.reeves@gmail.com>
2021-01-13 14:52:48 -0500

    [linked_list] Build out initial pieces of reverse function

--- linked_list.py
+++ linked_list.py
@@ -1,20 +1,32 @@
-node_one = {
-    next: node_two,
-    value: 1
+node_three = {
+    'next': None,
+    'value': 3
 }
 
 node_two = {
-    next: node_three,
-    value: 2
+    'next': node_three,
+    'value': 2
 }
 
-node_three = {
-    next: null,
-    value: 3
+node_one = {
+    'next': node_two,
+    'value': 1
 }
 
 def reverse(head):
-    return new_head
+    node_array = []
+    next_node = head
+    while next_node.next:
+        node_array.append(next_node)
+        next_node = next_node.next
+    node_array.append(next_node)
+    node_array = node_array.reverse()
+    for node in node_array:
+        node_array[node].next = node_array[node + 1]
+    return node_array
 
 # end result: three -> two -> one -> null
 # function takes in one as argument
+
+result = reverse(node_one)
+print(result)
